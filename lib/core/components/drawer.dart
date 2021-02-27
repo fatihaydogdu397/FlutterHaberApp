f84c33b19a26f8b85ea3986129fd227648f9ce91
fatih <45509775+fatihaydogdu397@users.noreply.github.com>
2021-02-27 18:38:31 +0300

    changes

--- lib/core/components/drawer.dart
+++ lib/core/components/drawer.dart
@@ -1,15 +1,43 @@
 import 'package:flutter/material.dart';
 
-
-
 class CustomDrawer extends StatelessWidget {
   const CustomDrawer({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Drawer(child:
-      Column(children: [],)
-        // All other codes goes here. 
-     );
+    return Drawer(
+        child: Column(
+      children: [
+        Container(),
+        SingleChildScrollView(
+          child: Column(
+            children: [DDButton()],
+          ),
+        )
+      ],
+    )
+        // All other codes goes here.
+        );
+  }
+}
+
+class DDButton extends StatelessWidget {
+  const DDButton({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: DropdownButton<String>(
+          elevation: 3,
+          hint: Text("asdad"),
+          
+      items: <String>['A', 'B', 'C', 'D'].map((String value) {
+        return new DropdownMenuItem<String>(
+          value: value,
+          child: new Text(value),
+        );
+      }).toList(),
+      onChanged: (_) {},
+    ));
   }
 }
